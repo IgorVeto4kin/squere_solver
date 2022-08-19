e4a1ed0fc0a84d997291b93c5876dfb9f130e18f
IgorVeto4kin <111492320+IgorVeto4kin@users.noreply.github.com>
2022-08-19 12:39:22 +0300

    Add files via upload
    перед чем-то осмысленным

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
+#define XYECOC main
+void fuck(void){
+    printf("\ngo fuck yourself\n");
+}
+int XYECOC()
+{
+    //int num;
+    //num = 5;
+    //printf(num, "\n");
+    //fuck();
+    long long a = pow(2, 19);
+    printf("%ld\n", a);
+    printf("%lld\n", a);
+    printf("%d\n", a);
+
+    //char ch;
+    //scanf("%c", &ch);
+    //printf("%d\n", num);//vot eto zaebicb
+    //char a = 'D';
+    //char x = 'A';
+    //printf("%c\t %c\n", a, x);
+    //printf("Koд символа %c равен %d.",ch,ch);
+    //getchar();
+    //double r;
+    //r = 4*2.0;
+    //printf("%e\n", r);
+    /*float a;
+    scanf("%f", &a);
+    printf("%e\n %f\n", a, a);
+    double b;
+    scanf("%f", &b);
+    printf("%e\n %f\n", b, b);*/
+    //b = 2.0e10 + 1;
+    //a = b - 2.0e10 ;
+    //printf ("%f \n", a ) ;
+    //printf("%d", EOF);
+    //char c = 'G';
+    //printf("%d\n", scanf("%d", &a));
+    //float c = 34.2;
+    //printf("%d\n%c\n%.20f\n",c,c,c);
+    /*float sal;
+    printf(" $_______\b\b\b\b\b\b\b") ;
+(x*sqrt(2))/3
+
+    scanf("%f", &sal);
+    printf("\n");
+    printf("%d, %f", sal, sal);
+    scanf("%f", &sal);*/
+    //unsigned
+    //printf("%d", printf("т"));
+    long long  q;
+    q = 111111111111;
+    //printf ("%f \n", FLT_EPSILON) ;
+    //printf("%ull", q);
+    //fuck();
+    //float a;
+    //double b;
+//    float a;
+    double b;
+  //  scanf("%f %lf", &a, &b);
+    //printf("%f    %lf\n%e    %e\n", a, b, a, b);
+    //scanf("", );
+    return 0;
+}
